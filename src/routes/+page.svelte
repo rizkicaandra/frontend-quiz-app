5017318925a84c8ace642b1b7e77cbb94a2a782d
mhd rizki candra <rizki.candra@ifortepay.id>
2025-07-20 23:04:47 +0700

    fixing background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main
-	class="font-family-regular min-h-screen w-full bg-[url('/images/pattern-background-mobile-light.svg')] bg-cover bg-center transition-colors duration-600 ease-in-out md:bg-[url('/images/pattern-background-tablet-light.svg')] md:bg-auto md:bg-top-left md:bg-no-repeat lg:bg-[url('/images/pattern-background-desktop-light.svg')] lg:bg-cover lg:bg-center dark:bg-[url('/images/pattern-background-mobile-dark.svg')] md:dark:bg-[url('/images/pattern-background-tablet-dark.svg')] lg:dark:bg-[url('/images/pattern-background-desktop-dark.svg')]"
+	class="font-family-regular min-h-screen w-full "
 >
 	<Header />
 	<StartMenu />
